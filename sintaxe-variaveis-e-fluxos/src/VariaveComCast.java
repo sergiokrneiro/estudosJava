4908e3abb970865a499661c2716e43f77670358a
sergiokrneiro <sergiokrneiro@gmail.com>
2019-10-24 09:40:17 -0300

    Códigos e conceitos iniciais de Java

---
+++ sintaxe-variaveis-e-fluxos/src/VariaveComCast.java
@@ -0,0 +1,15 @@
+
+public class VariaveComCast {
+
+	//Programa para exemplificar utilizacao do cast.
+	public static void main(String[] args) {
+		
+		double numeroDecimal = 9.97;
+		
+		int recebeDouble = (int)numeroDecimal;
+		
+		System.out.println(recebeDouble);
+		
+	}
+
+}
---
+++ sintaxe-variaveis-e-fluxos/src/Variaveis.java
@@ -0,0 +1,36 @@
+
+public class Variaveis {
+	
+	public static void main(String[] args) {
+		
+		int idade = 20;
+		
+		System.out.println("A idade é: " + idade + " anos.");
+		
+		double salario = 3890.87;
+		
+		System.out.println("O Salário é: R$ " + salario);
+		
+		double divisaoDoublePorInt;
+		double divisaoIntPorDouble;
+		int numero1 = 10;
+		double numero2 = 3.14;
+		
+		//A variavel que receberá a divisao deve ser do tipo double e pelo menos uma das variaves do calculo deverar se do tipo double.
+		
+		divisaoDoublePorInt = (numero2 / numero1);
+		System.out.println("Resultado da divisao: " + divisaoDoublePorInt);
+		
+		divisaoIntPorDouble = (numero1 / numero2);
+		System.out.println("Resultado da divisao: " + divisaoIntPorDouble);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
