d9151d6432d13851613b6ffd63f4d6afc10a5d17
sergiokrneiro <sergiokrneiro@gmail.com>
2019-10-26 08:50:09 -0300

    Ińicio de OO

---
+++ orientacaoabjetos-bytebank/src/CriaConta.java
@@ -0,0 +1,23 @@
+
+public class CriaConta {
+	
+	public static void main(String[] args) {
+		//Instanciacao:
+
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 200;
+		System.out.println("Saldo da primeira conta: R$ " + primeiraConta.saldo);
+		System.out.println(" ");
+		
+		Conta segundaConta = new Conta();
+		segundaConta.agencia = 2123;
+		segundaConta.conta = 546398;
+		segundaConta.titular = "Joaquim José da Silva Xavier";
+		segundaConta.saldo = 357689.36;
+		System.out.println("Dados da segunda conta: ");	
+		System.out.print("Agencia: " + segundaConta.agencia + "\nConta corrente: " + segundaConta.conta + " \nTitular: " + segundaConta.titular + " \nSaldo: R$ " + segundaConta.saldo);
+		
+		
+	}
+
+}
