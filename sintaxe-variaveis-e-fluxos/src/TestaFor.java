76372877bafe30116d4521c720645a94dd88d3d9
sergiokrneiro <sergiokrneiro@gmail.com>
2019-10-26 07:45:24 -0300

    Mais sobre laços

---
+++ sintaxe-variaveis-e-fluxos/src/TestaFor.java
@@ -0,0 +1,11 @@
+
+public class TestaFor {
+	
+	public static void main(String[] args) {
+		
+		for(int i = 0; i <= 8; i++) {
+			System.out.println("A contatem está em: " + i);
+		}
+	}
+
+}
---
+++ sintaxe-variaveis-e-fluxos/src/TestaWhile.java
@@ -0,0 +1,16 @@
+
+public class TestaWhile {
+	
+	public static void main(String[] args) {
+		
+		int i = 0;
+		
+		while (i <= 20) {
+			System.out.println("Contador equivale a : " + i);
+			i++;
+		}
+		
+		
+	}
+
+}
